785c4d5b90bad0eba182d71c31c1b2001e4c454e
unknown <alisonguillen@outlook.com>
2023-10-20 13:13:07 -0600

    prmer commit

---
+++ Lab2P1_AlisonGuillen/src/lab2p1_alisonguillen/Lab2P1_AlisonGuillen.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab2p1_alisonguillen;
+
+/**
+ *
+ * @author Dell i7
+ */
+public class Lab2P1_AlisonGuillen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
